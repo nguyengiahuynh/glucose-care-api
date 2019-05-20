010b8ede3fce11730aff0950234929ac2d966a5c
nguyengiahuynh <1512214@student.hcmus.edu.vn>
2019-05-20 15:39:20 +0700

    fix api chat, add get statistic 7 latest days

--- mySQL/glucose_care.sql
+++ mySQL/glucose_care.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100138
  File Encoding         : 65001
 
- Date: 19/05/2019 12:35:33
+ Date: 20/05/2019 15:37:38
 */
 
 SET NAMES utf8mb4;
@@ -74,24 +74,28 @@ DROP TABLE IF EXISTS `chat`;
 CREATE TABLE `chat`  (
   `MaDoanChat` bigint(255) NOT NULL AUTO_INCREMENT,
   `MaNguoiGui` varchar(10) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NULL DEFAULT NULL,
+  `LoaiNguoiGui` tinyint(255) NULL DEFAULT NULL,
   `MaNguoiNhan` varchar(10) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NULL DEFAULT NULL,
+  `LoaiNguoiNhan` tinyint(255) NULL DEFAULT NULL,
   `NoiDung` varchar(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NULL DEFAULT NULL,
   `NgayGioGui` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`MaDoanChat`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of chat
 -- ----------------------------
-INSERT INTO `chat` VALUES (1, '0941430622', '0982861197', 'OK đã gửi được', '2019-06-30 14:35:41');
-INSERT INTO `chat` VALUES (2, '0941430622', '0982861197', 'OKKK', '2019-04-09 11:48:38');
-INSERT INTO `chat` VALUES (3, '0982861197', '0941430622', 'Replied', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (4, '123456789', '0982860738', 'Test Socket', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (5, '123456789', '0982860738', 'hello', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (6, '123456789', '0982860738', 'Socket chạy được rồi nè', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (7, '123456789', '0982860738', 'OK luôn nha', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (8, '123456789', '0982860738', 'hi hi', '2019-05-01 07:51:32');
-INSERT INTO `chat` VALUES (9, '123456789', '0982860738', 'Huỳnh', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (1, '0941430622', 1, '0982861197', 2, 'OK đã gửi được', '2019-06-30 14:35:41');
+INSERT INTO `chat` VALUES (2, '0941430622', 1, '0982861197', 2, 'OKKK', '2019-04-09 11:48:38');
+INSERT INTO `chat` VALUES (3, '0982861197', 2, '0941430622', 1, 'Replied', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (4, '123456789', 1, '0982860738', 1, 'Test Socket', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (5, '123456789', 1, '0982860738', 1, 'hello', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (6, '123456789', 1, '0982860738', 1, 'Socket chạy được rồi nè', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (7, '123456789', 1, '0982860738', 1, 'OK luôn nha', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (8, '123456789', 2, '0982860738', 2, 'hi hi', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (9, '123456789', 2, '0982860738', 2, 'Huỳnh', '2019-05-01 07:51:32');
+INSERT INTO `chat` VALUES (10, '0941430622', 2, '0982861197', 2, 'BN chat với nhau', '2019-05-20 15:10:03');
+INSERT INTO `chat` VALUES (11, '0982861197', 2, '0941430622', 2, 'BN chat', '2019-05-21 15:10:50');
 
 -- ----------------------------
 -- Table structure for chi_so
@@ -100,19 +104,36 @@ DROP TABLE IF EXISTS `chi_so`;
 CREATE TABLE `chi_so`  (
   `MaKetQua` bigint(255) NOT NULL AUTO_INCREMENT,
   `MaBenhNhan` varchar(10) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NULL DEFAULT NULL,
-  `Loai` tinyint(255) NULL DEFAULT NULL,
+  `Loai` double(255, 1) NULL DEFAULT NULL,
   `ChiSo` double(255, 2) NULL DEFAULT NULL,
   `NgayNhap` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`MaKetQua`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of chi_so
 -- ----------------------------
-INSERT INTO `chi_so` VALUES (3, '0941430622', 1, 23.50, '2019-06-20 14:35:41');
-INSERT INTO `chi_so` VALUES (4, '0941430622', 2, 50.00, '2019-06-20 14:40:41');
-INSERT INTO `chi_so` VALUES (5, '0941430622', 3, 90.00, '2019-06-15 14:40:41');
-INSERT INTO `chi_so` VALUES (6, '0941430622', 2, 180.00, '2019-06-10 14:40:41');
+INSERT INTO `chi_so` VALUES (3, '0941430622', 2.1, 23.50, '2019-06-20 14:35:41');
+INSERT INTO `chi_so` VALUES (4, '0941430622', 2.1, 50.00, '2019-06-20 14:40:41');
+INSERT INTO `chi_so` VALUES (5, '0941430622', 2.1, 90.00, '2019-06-15 14:40:41');
+INSERT INTO `chi_so` VALUES (6, '0941430622', 2.1, 180.00, '2019-06-10 14:40:41');
+INSERT INTO `chi_so` VALUES (7, '0941430622', 2.1, 70.00, '2019-06-15 17:13:52');
+INSERT INTO `chi_so` VALUES (8, '0941430622', 2.1, 10.00, '2019-05-20 10:49:21');
+INSERT INTO `chi_so` VALUES (9, '0941430622', 2.1, 30.00, '2019-05-22 10:56:50');
+INSERT INTO `chi_so` VALUES (10, '0941430622', 2.1, 55.00, '2019-05-24 11:09:22');
+INSERT INTO `chi_so` VALUES (11, '0941430622', 2.1, 60.00, '2019-05-28 11:09:43');
+INSERT INTO `chi_so` VALUES (12, '0941430622', 2.1, 80.00, '2019-05-31 11:10:23');
+INSERT INTO `chi_so` VALUES (14, '0941430622', 2.2, 10.00, '2019-06-20 14:35:41');
+INSERT INTO `chi_so` VALUES (15, '0941430622', 2.2, 20.00, '2019-06-20 14:40:41');
+INSERT INTO `chi_so` VALUES (16, '0941430622', 2.2, 30.00, '2019-06-15 14:40:41');
+INSERT INTO `chi_so` VALUES (17, '0941430622', 2.2, 40.00, '2019-06-10 14:40:41');
+INSERT INTO `chi_so` VALUES (18, '0941430622', 2.2, 50.00, '2019-06-15 17:13:52');
+INSERT INTO `chi_so` VALUES (19, '0941430622', 2.2, 60.00, '2019-05-20 10:49:21');
+INSERT INTO `chi_so` VALUES (20, '0941430622', 2.2, 70.00, '2019-05-22 10:56:50');
+INSERT INTO `chi_so` VALUES (21, '0941430622', 2.2, 80.00, '2019-05-24 11:09:22');
+INSERT INTO `chi_so` VALUES (22, '0941430622', 2.2, 90.00, '2019-05-28 11:09:43');
+INSERT INTO `chi_so` VALUES (23, '0941430622', 2.2, 100.00, '2019-05-31 11:10:23');
+INSERT INTO `chi_so` VALUES (24, '0941430622', 1.0, 50.00, '2019-05-20 14:21:36');
 
 -- ----------------------------
 -- Table structure for sessions
@@ -129,7 +150,7 @@ CREATE TABLE `sessions`  (
 -- Records of sessions
 -- ----------------------------
 INSERT INTO `sessions` VALUES ('_-RI_2V2mYy3i2jNDDiBHvpPmf0_Yz9T', 1559895938, '{\"cookie\":{\"originalMaxAge\":2592000000,\"expires\":\"2019-06-07T02:24:14.136Z\",\"secure\":false,\"httpOnly\":false,\"path\":\"/\"},\"IsDoctorLogged\":true,\"Doctor\":{\"MaBacSi\":\"123456789\",\"Password\":\"565339bc4d33d72817b583024112eb7f5cdf3e5eef0252d6ec1b9c9a94e12bb3\",\"HoTen\":\"Nguyễn Hoài Nam\"},\"IsPatientLogged\":true,\"Patient\":{\"MaBenhNhan\":\"0941430622\",\"Password\":\"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08\",\"HoTen\":\"Nguyễn Gia Huỳnh\"}}');
-INSERT INTO `sessions` VALUES ('dLQ1qbTaYZdiUNU-7tfbSDScVu4OxmzE', 1560571238, '{\"cookie\":{\"originalMaxAge\":2592000000,\"expires\":\"2019-06-15T03:47:14.132Z\",\"secure\":false,\"httpOnly\":false,\"path\":\"/\"},\"IsPatientLogged\":true,\"Patient\":{\"MaBenhNhan\":\"0939977538\",\"Password\":\"329f44354267705eeefe8321bb5bff5caabe25e34654855cce1306fca2262868\",\"HoTen\":\"Lưu Khởi Toàn\"}}');
+INSERT INTO `sessions` VALUES ('dLQ1qbTaYZdiUNU-7tfbSDScVu4OxmzE', 1560933371, '{\"cookie\":{\"originalMaxAge\":2592000000,\"expires\":\"2019-06-15T03:47:14.132Z\",\"secure\":false,\"httpOnly\":false,\"path\":\"/\"},\"IsPatientLogged\":true,\"Patient\":{\"MaBenhNhan\":\"0939977538\",\"Password\":\"329f44354267705eeefe8321bb5bff5caabe25e34654855cce1306fca2262868\",\"HoTen\":\"Lưu Khởi Toàn\"}}');
 
 -- ----------------------------
 -- Table structure for theo_doi
@@ -143,9 +164,9 @@ CREATE TABLE `theo_doi`  (
   `IsRequest` tinyint(1) NULL DEFAULT NULL,
   `IsFollow` tinyint(1) NULL DEFAULT NULL,
   PRIMARY KEY (`Id`) USING BTREE,
-  INDEX `NguoiBiTheoDoi`(`NguoiBiTheoDoi`) USING BTREE,
-  INDEX `theo_doi_ibfk_2`(`NguoiTheoDoi`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  INDEX `theo_doi_ibfk_2`(`NguoiTheoDoi`) USING BTREE,
+  INDEX `NguoiBiTheoDoi`(`NguoiBiTheoDoi`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of theo_doi
--- routes/statistics.js
+++ routes/statistics.js
@@ -7,114 +7,46 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
     var MaBenhNhan = req.query.MaBenhNhan;
-    var Loai = req.query.Loai;
-    var page = req.query.page;
-    if (!page) {
-        page = 1;
+    var Loai = +req.query.Loai;
+    if (Loai === 1) {
+        statisticsRepo.load7LatestDaysOfStatistic(1, MaBenhNhan).then(result => {
+            return res.status(200).json({
+                blood_sugar: result,
+                length: result.length,
+                status: 'success'
+            })
+        }).catch((err) => {
+            return res.status(200).json({
+                status: 'failed',
+                message_error: err
+            })
+        })
     }
-    var isFirstPage = false,
-        isLastPage = false;
-    var current_page;
-    var offset = (page - 1) * constants.STATISTICS_PER_PAGE;
-    var p1 = statisticsRepo.loadAllStatistics(offset, Loai, MaBenhNhan);
-    var p2 = statisticsRepo.countStatistics(Loai, MaBenhNhan);
-    Promise.all([p1, p2]).then(([rows, countRows]) => {
-        var total = countRows[0].total;
-        var nPages = parseInt(total / constants.STATISTICS_PER_PAGE);
-        if (total % constants.STATISTICS_PER_PAGE > 0) {
-            nPages++;
-        }
-
-        if (+page === 1)
-            isFirstPage = true;
-        if (+page === nPages)
-            isLastPage = true;
-
-        var numbers = [];
-        if (nPages > 2) {
-            if (+page === 1)
-                for (var i = 1; i <= 3; i++)
-                    numbers.push({
-                        value: i,
-                        isCurPage: i === +page,
-                    });
-            else if (+page === nPages)
-                for (var i = nPages - 2; i <= nPages; i++)
-                    numbers.push({
-                        value: i,
-                        isCurPage: i === +page
-                    });
-            else {
-                numbers.push({
-                    value: +page - 1,
-                    isCurPage: false
-                });
-                numbers.push({
-                    value: +page,
-                    isCurPage: true
-                });
-                numbers.push({
-                    value: +page + 1,
-                    isCurPage: false
-                });
-            }
-        } else if (nPages < 2) {
-            isFirstPage = true;
-            isLastPage = true;
-            numbers.push({
-                value: +page,
-                isCurPage: true
-            });
-        } else {
-            if (+page === 1) {
-                isFirstPage = true;
-                isLastPage = false;
-                numbers.push({
-                    value: +page,
-                    isCurPage: true
-                });
-                numbers.push({
-                    value: +page + 1,
-                    isCurPage: false
-                });
-            } else {
-                isFirstPage = false;
-                isLastPage = true;
-                numbers.push({
-                    value: +page - 1,
-                    isCurPage: false
-                });
-                numbers.push({
-                    value: +page,
-                    isCurPage: true
-                });
-            }
-        }
-
-        for (var i = 0; i < numbers.length; i++)
-            if (numbers[i].isCurPage == true) {
-                current_page = numbers[i].value;
-                break;
-            }
-
-        return res.status(200).json({
-            total_statistic: total,
-            statistics: rows,
-            no_statistics: rows.length === 0,
-            page_numbers: numbers,
-            last_page: nPages,
-            is_first_page: isFirstPage,
-            is_last_page: isLastPage,
-            total_page: nPages,
-            current_page,
-            status: 'success'
+    else if (Loai === 2) {
+        var p1 = statisticsRepo.load7LatestDaysOfStatistic(2.1, MaBenhNhan);
+        var p2 = statisticsRepo.load7LatestDaysOfStatistic(2.2, MaBenhNhan);
+        var result = [];
+        Promise.all([p1, p2]).then(([rows01, rows02]) => {
+            rows01.map((bloodPressure01, index) => {
+                const  bloodPressure02 =  rows02[index];
+                var temp = {
+                    blood_pressure01: bloodPressure01,
+                    blood_pressure02: bloodPressure02
+                }
+                result.push(temp)
+            })
+            return res.status(200).json({
+                blood_pressure: result,
+                length: result.length,
+                status: 'success'
+            })
+        }).catch((err) => {
+            return res.status(200).json({
+                status: 'failed',
+                message_error: err
+            })
         })
-    }).catch((err) => {
-        return res.status(200).json({
-            status: 'failed',
-            message_error: err
-        })
-    })
+    }
 });
 
 router.post('/add', (req, res) => {
