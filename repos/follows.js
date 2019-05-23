28a4332642c411384c80a9620781d6e487e64395
manhvv <manhvv@evolableasia.vn>
2019-05-23 10:55:38 +0700

    fix get following doctorr

--- repos/follows.js
+++ repos/follows.js
@@ -44,7 +44,7 @@ exports.getListPatientFollowed = (MaBenhNhan) => {
 }
 
 exports.getListDoctorFollowing = (MaBacSi) => {
-    var sql = `select bn.MaBenhNhan, bn.HoTen, bn.Avatar, bn.CMND, bn.GioiTinh, bn.DiaChi, bn.Email, bn.NgaySinh, bn.NgheNghiep, bn.NhomMau, bn.DiUngThuoc from benh_nhan bn, theo_doi td where bn.MaBenhNhan = td.NguoiBiTheoDoi and IsFollow = 1 and NguoiTheoDoi = '${MaBacSi}' and bn.IsDeleted = 0 and Loai = '${Loai}'`;
+    var sql = `select bn.MaBenhNhan, bn.HoTen, bn.Avatar, bn.CMND, bn.GioiTinh, bn.DiaChi, bn.Email, bn.NgaySinh, bn.NgheNghiep, bn.NhomMau, bn.DiUngThuoc from benh_nhan bn, theo_doi td where bn.MaBenhNhan = td.NguoiBiTheoDoi and IsFollow = 1 and NguoiTheoDoi = '${MaBacSi}' and bn.IsDeleted = 0`;
     return db.load(sql);
 }
 
