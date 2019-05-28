f47604436de4568ce0d3f6215ee67fce32460c78
manhvv <manhvv@evolableasia.vn>
2019-05-28 09:52:22 +0700

    fix follow doctor

--- routes/follows.js
+++ routes/follows.js
@@ -308,15 +308,15 @@ router.get('/check-relationship-of-patient-with-doctor', function (req, res, nex
             })
         }
         if (rows2.length > 0) {
-            if (rows2[0].NguoiTheoDoi === req.query.MaBenhNhan2) {
+            if (rows2[0].NguoiTheoDoi === req.query.MaBacSi) {
                 return res.status(200).json({
-                    typeRelationship: 'accept',
+                    typeRelationship: 'cancel',
                     status: 'success'
                 })
             }
             else {
                 return res.status(200).json({
-                    typeRelationship: 'cancel',
+                    typeRelationship: 'accept',
                     status: 'success'
                 })
             }
